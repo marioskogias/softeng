a042227ce9ee9b03614d1eb2810197f2c2a73f53
Marios Kogias <marioskogias@gmail.com>
2014-04-17 11:29:07 +0300

    Add register button in Authentication splash

--- description-and-code-base/SoftEng/SipCommunicator-Fall05/net/java/sip/communicator/gui/AuthenticationSplash.java
+++ description-and-code-base/SoftEng/SipCommunicator-Fall05/net/java/sip/communicator/gui/AuthenticationSplash.java
@@ -113,11 +113,18 @@ public class AuthenticationSplash
      */
     private String CMD_LOGIN = "cmd.login" /*NOI18N*/;
 
+    /**
+     * Command string for a register action (e.g., a button).
+     * This string is never presented to the user and should
+     * not be internationalized.
+     */
+    private String CMD_REGISTER = "cmd.register" /*NOI18N*/;
+
     // Components we need to manipulate after creation
     private JButton loginButton = null;
     private JButton cancelButton = null;
     private JButton helpButton = null;
-
+    private JButton registerButton = null;
     /**
      * Creates new form AuthenticationSplash
      */
@@ -367,6 +374,22 @@ public class AuthenticationSplash
             }
         });
         //buttonPanel.add(helpButton);
+
+        // register
+        buttonPanel.add(Box.createRigidArea(new Dimension(5, 0)));
+
+        registerButton = new JButton();
+        registerButton.setText("Register");
+        registerButton.setActionCommand(CMD_REGISTER);
+        registerButton.addActionListener(new ActionListener()
+        {
+            public void actionPerformed(ActionEvent event)
+            {
+                dialogDone(event);
+            }
+        });
+        buttonPanel.add(registerButton);
+
         c = new GridBagConstraints();
         c.gridx = 0;
         c.gridy = 3;
