c97b1a835a5498936434b8041fb57eb3fafe3307
Marios Kogias <marioskogias@grnet.gr>
2014-04-26 19:43:04 +0300

    Add ForwardingClient class
    This client will be responsible for communicating with the
    forwarding manager.

---
+++ description-and-code-base/SoftEng/SipCommunicator-Fall05/net/java/sip/communicator/forwardService/ForwardClient.java
@@ -0,0 +1,12 @@
+package net.java.sip.communicator.forwardService;
+
+public class ForwardClient {
+
+	public String getForward(String username) {
+		return "forward_to";
+	}
+	
+	public void forwardTo(String toUser){
+		
+	}
+}
