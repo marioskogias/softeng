c1bb86a240fcbd3090347f71b55acf663f3bea10
Marios Kogias <marioskogias@grnet.gr>
2014-04-27 14:37:46 +0300

    Set the layout of the proxy and forwardingService

--- description-and-code-base/SoftEng/sipproxy/gov/nist/sip/proxy/additionalServices/ForwardingService.java
+++ description-and-code-base/SoftEng/sipproxy/gov/nist/sip/proxy/additionalServices/ForwardingService.java
@@ -1,5 +1,44 @@
 package gov.nist.sip.proxy.additionalServices;
 
-public class forwardingService {
+import java.text.ParseException;
 
+import gov.nist.sip.proxy.Proxy;
+
+import javax.sip.address.URI;
+import javax.sip.header.FromHeader;
+import javax.sip.header.ToHeader;
+import javax.sip.message.Request;
+
+public class ForwardingService {
+
+	Proxy proxy;
+
+	public ForwardingService(Proxy p) {
+		this.proxy = p;
+	}
+
+	public Request checkAndSetForwarding(Request request) {
+
+		FromHeader header = (FromHeader) request.getHeader(FromHeader.NAME);
+		if (header.getAddress().getURI().toString().contains("marios2")) {
+
+			URI n;
+			ToHeader newTo = null;
+			try {
+				n = proxy.getAddressFactory().createURI(
+						"sip:marios3@192.168.1.6:4000");
+				newTo = proxy.getHeaderFactory().createToHeader(
+						proxy.getAddressFactory().createAddress(n), null);
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			// request.setRequestURI(n);
+			Request newreq = (Request) request.clone();
+			newreq.setHeader(newTo);
+			System.out.println("in my forward");
+			return newreq;
+		}
+		return null;
+	}
 }
