41c50e9f45628f27a088e1e8ebb9c0edc4ff3a57
Marios Kogias <marioskogias@grnet.gr>
2014-04-27 18:47:23 +0300

    Forwarding ready from proxy side

--- description-and-code-base/SoftEng/sipproxy/gov/nist/sip/proxy/additionalServices/ForwardingService.java
+++ description-and-code-base/SoftEng/sipproxy/gov/nist/sip/proxy/additionalServices/ForwardingService.java
@@ -2,6 +2,7 @@ package gov.nist.sip.proxy.additionalServices;
 
 import java.text.ParseException;
 
+import gov.nist.sip.db.ForwardDB;
 import gov.nist.sip.proxy.Proxy;
 
 import javax.sip.address.URI;
@@ -12,33 +13,46 @@ import javax.sip.message.Request;
 public class ForwardingService {
 
 	Proxy proxy;
+	ForwardDB dbManager;
 
 	public ForwardingService(Proxy p) {
 		this.proxy = p;
+		dbManager = new ForwardDB();
+	}
+
+	private String getUsernameFromHeader(ToHeader header) {
+		URI uri = header.getAddress().getURI();
+		String uriString = uri.toString();
+		return uriString.substring(uriString.indexOf("sip:") + 4,
+				uriString.indexOf("@"));
 	}
 
 	public Request checkAndSetForwarding(Request request) {
 
-		FromHeader header = (FromHeader) request.getHeader(FromHeader.NAME);
-		if (header.getAddress().getURI().toString().contains("marios2")) {
-
-			URI n;
-			ToHeader newTo = null;
+		ToHeader header = (ToHeader) request.getHeader(ToHeader.NAME);
+		String oldToUser = getUsernameFromHeader(header);
+		String toUser = dbManager.getForward(oldToUser);
+		if (toUser != null) {
+			String originalUri = header.getAddress().toString();
+			String newUri = "sip:" + toUser
+					+ originalUri.substring(originalUri.indexOf("@"));
+			URI newURI;
 			try {
-				n = proxy.getAddressFactory().createURI(
-						"sip:marios3@192.168.1.6:4000");
-				newTo = proxy.getHeaderFactory().createToHeader(
-						proxy.getAddressFactory().createAddress(n), null);
+				newURI = proxy.getAddressFactory().createURI(newUri);
+				ToHeader newTo = proxy.getHeaderFactory().createToHeader(
+						proxy.getAddressFactory().createAddress(newURI), null);
+				Request newreq = (Request) request.clone();
+				newreq.setHeader(newTo);
+				return newreq;
+
 			} catch (ParseException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			// request.setRequestURI(n);
-			Request newreq = (Request) request.clone();
-			newreq.setHeader(newTo);
-			System.out.println("in my forward");
-			return newreq;
-		}
+
+		} else
+			return request;
+		
 		return null;
 	}
 }
