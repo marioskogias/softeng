1a78db602e7bc70687a21d4d65824da6d02329b8
Marios Kogias <marioskogias@grnet.gr>
2014-04-27 19:18:29 +0300

    SipCommunicator: Get forward from db

--- description-and-code-base/SoftEng/SipCommunicator-Fall05/net/java/sip/communicator/db/ForwardDB.java
+++ description-and-code-base/SoftEng/SipCommunicator-Fall05/net/java/sip/communicator/db/ForwardDB.java
@@ -1,24 +1,47 @@
 package net.java.sip.communicator.db;
 
 import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
 public class ForwardDB {
 	private ParseXMLCredentials dbCred;
 
 	Connection conn = null;
-	Statement stmt = null;
+	PreparedStatement stmt = null;
 
 	public ForwardDB() {
 		dbCred = new ParseXMLCredentials();
 	}
-	
+
 	public String getForward(String username) {
-		return "forward_to";
+		String forwardTo = "";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbCred.getUrl(),
+					dbCred.getUsername(), dbCred.getPassword());
+			stmt = conn
+					.prepareStatement("SELECT forwardTo FROM forwarding where forwardFrom = ?");
+
+			stmt.setString(1, username);
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				forwardTo = rs.getString("forwardTo");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		return forwardTo;
 	}
-	
-	public void setForward(String fromUser, String toUser)  throws NoSuchElementException, RuntimeException{
+
+	public void setForward(String fromUser, String toUser)
+			throws NoSuchElementException, RuntimeException {
 		throw new RuntimeException();
 	}
 }
