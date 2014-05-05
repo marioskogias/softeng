d70ec2e3722e0fd3b5d915928bcf70c3069be203
Marios Kogias <marioskogias@grnet.gr>
2014-05-05 12:49:34 +0300

    Add all sql tables

--- description-and-code-base/SoftEng/sql/dump.sql
+++ description-and-code-base/SoftEng/sql/dump.sql
@@ -24,6 +24,53 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/ `sip` /*!40100 DEFAULT CHARACTER SET la
 USE `sip`;
 
 --
+-- Table structure for table `billing`
+--
+
+DROP TABLE IF EXISTS `billing`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `billing` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `start_time` bigint(20) DEFAULT NULL,
+  `duration` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `billing`
+--
+
+LOCK TABLES `billing` WRITE;
+/*!40000 ALTER TABLE `billing` DISABLE KEYS */;
+/*!40000 ALTER TABLE `billing` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `blocking`
+--
+
+DROP TABLE IF EXISTS `blocking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `blocking` (
+  `blockedFrom` varchar(30) NOT NULL,
+  `blocked` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blocking`
+--
+
+LOCK TABLES `blocking` WRITE;
+/*!40000 ALTER TABLE `blocking` DISABLE KEYS */;
+/*!40000 ALTER TABLE `blocking` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `forwarding`
 --
 
@@ -59,7 +106,7 @@ CREATE TABLE `users` (
   `creditCard` varchar(100) DEFAULT NULL,
   `email` varchar(60) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -80,4 +127,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-05-04 19:12:12
+-- Dump completed on 2014-05-05 12:46:14
