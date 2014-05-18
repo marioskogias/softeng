ee0e0082b8bdfc40c4bd47c19cc1af7a3a3a8c7f
Marios Kogias <marioskogias@gmail.com>
2014-05-18 11:48:01 +0300

    Add billing plan combo box

--- description-and-code-base/SoftEng/SipCommunicator-Fall05/net/java/sip/communicator/gui/RegistrationSplash.java
+++ description-and-code-base/SoftEng/SipCommunicator-Fall05/net/java/sip/communicator/gui/RegistrationSplash.java
@@ -75,7 +75,7 @@ import net.java.sip.communicator.common.*;
 /**
  * Sample login splash screen
  */
-public class RegistrationSplash extends JDialog {
+public class RegistrationSplash extends JDialog implements ActionListener {
 	String userName = null;
 	char[] password = null;
 	String mail = null;
@@ -312,6 +312,32 @@ public class RegistrationSplash extends JDialog {
 		c.insets = new Insets(12, 7, 0, 11);
 		centerPane.add(creditCardTextField, c);
 		
+		//Billing plans combo
+		
+		JLabel plans = new JLabel("Billing Plan");
+		c = new GridBagConstraints();
+		c.gridx = 0;
+		c.gridy = gridy;
+		c.anchor = GridBagConstraints.WEST;
+		c.insets = new Insets(12, 12, 0, 0);
+		centerPane.add(plans, c);
+		
+		String[] planStrings = { "Plain", "Friends and Family", "Free Sunday" };
+
+		//Create the combo box, select item at index 4.
+		//Indices start at 0, so 4 specifies the pig.
+		JComboBox plansList = new JComboBox(planStrings);
+		plansList.setSelectedIndex(0);
+		plansList.addActionListener(this);
+		
+		c = new GridBagConstraints();
+		c.gridx = 1;
+		c.gridy = gridy++;
+		c.anchor = GridBagConstraints.WEST;
+		c.insets = new Insets(12, 12, 0, 0);
+		centerPane.add(plansList, c);
+		
+		
 		// Buttons along bottom of window
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new BoxLayout(buttonPanel, 0));
@@ -384,5 +410,11 @@ public class RegistrationSplash extends JDialog {
 			else
 				return passwordTextField;
 		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 } // class LoginSplash
