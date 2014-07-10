b13ae5ab3cbdd8b06952311ddaf81afc7544d72f
TDaglis <thanasis@sourcelair.com>
2014-07-10 19:53:51 +0300

    fixed labels of friend splash

--- description-and-code-base/SoftEng/SipCommunicator-Fall05/net/java/sip/communicator/gui/FriendSplash.java
+++ description-and-code-base/SoftEng/SipCommunicator-Fall05/net/java/sip/communicator/gui/FriendSplash.java
@@ -102,7 +102,6 @@ public class FriendSplash extends JDialog {
 		relationLabel.setDisplayedMnemonic('Y');
 		// setLabelFor() allows the mnemonic to work
 		relationLabel.setLabelFor(friendRelationTextField);
-		relationLabel.setLabelFor(friendRelationTextField);
 		
 		int gridy = 0;
 		JLabel friendListLabel = new JLabel();
@@ -139,13 +138,13 @@ public class FriendSplash extends JDialog {
 		centerPane.add(friendControlTextField, c);
 		
 		
-		relationLabel.setText("relation with user:");
+		relationLabel.setText("Relation with user(friend/family):");
 		c = new GridBagConstraints();
 		c.gridx = 0;
 		c.gridy = gridy;
 		c.anchor = GridBagConstraints.WEST;
 		c.insets = new Insets(12, 12, 0, 0);
-		centerPane.add(removeLabel, c);
+		centerPane.add(relationLabel, c);
 		// user name text
 		c = new GridBagConstraints();
 		c.gridx = 1;
